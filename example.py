fb2f8759e01dbf0b931e043a54479a5ecbd03bb2
Akashbr-2006 <akash.b.renuka@gmail.com>
2024-08-10 21:52:16 +0530

    updating
    added a new file for user login

--- example.py
+++ example.py
@@ -2,9 +2,9 @@ import pymysql
 connectin=pymysql.connect(host='localhost',
                           user='root',
                           password='1#nmnmnm1#',
-                          database='test')
+                          database='booking_system')
 Coursor=connectin.cursor()
-Coursor.execute("select * from student;")
+Coursor.execute("select * from seat_info;")
 data=Coursor.fetchall()
 for item in data:
     print(item[0])
---
+++ user_login_window.py
@@ -0,0 +1,11 @@
+import pymysql
+import tkinter
+
+
+#sql connection
+
+Connection=pymysql.connect(host='localhost',
+                           user='root',
+                           password='1#nmnmnm1#',
+                           database='test'
+                           )
