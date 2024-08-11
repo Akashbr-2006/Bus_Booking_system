19aae222e106491e18651f4215cb552edeebf60a
Akashbr-2006 <akash.b.renuka@gmail.com>
2024-08-11 17:50:36 +0530

    Update Busbooking.py

--- Busbooking.py
+++ Busbooking.py
@@ -5,8 +5,8 @@ def update_status(seat_number):
     Connection=pymysql.connect(host='localhost',
                           user='root',
                           password='1#nmnmnm1#',
-                          database='test')
+                          database='booking_system')
     Coursor=Connection.cursor()
-    Coursor.execute("SELECT * FROM test.seat_info where status = 0;")
+    Coursor.execute("SELECT * FROM booking_system.seat_info;")
     print(Coursor.fetchall())
 update_status(1)
