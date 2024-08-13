edcd3eeb4ea1cccbde711c75a9ab520fb28d2805
Akashbr-2006 <akash.b.renuka@gmail.com>
2024-08-14 00:07:02 +0530

    Update temp.py

---
+++ temp.py
@@ -0,0 +1,8 @@
+import tkinter 
+ 
+window=tkinter.Tk()
+window.title("XYZ Bus booking")
+window.geometry("600x600")
+window.configure(background="#21201c")
+new_account_frame=tkinter.Frame(window,background="#21201c")
+new_account_frame
